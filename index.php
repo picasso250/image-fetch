7d3c354a4ec4059d1833b83f53a8a4876317642e
picasso250 <cumt.xiaochi@gmail.com>
2014-09-14 01:58:02 +0800

    fix

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
-$attitude_map = json_decode(file_get_contents('attitude.json'), true);
+function get_data($name)
+{
+    return json_decode(file_get_contents($name.'.json'), true);
+}
+
+$attitude_map = get_data('attitude');
 
 /* AJAX check  */
 if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
@@ -8,6 +13,9 @@ if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQU
     $action = $_POST['action'][0];
     $file = $_POST['file'];
     $attitude_map[$file] = $action;
+    if ($action === 'd') {
+        unlink($file);
+    }
     file_put_contents('attitude.json', json_encode($attitude_map));
     error_log("$action $file\n", 3, 'show.log');
     die(json_encode(array('code' => 0)));
@@ -15,11 +23,11 @@ if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQU
 
 $d = dir("images");
 include 'head.html';
-echo '<link rel="stylesheet" type="text/css" href="style.css">';
+$image_page = get_data('image_page');
 while (false !== ($entry = $d->read())) {
     $f = "images/$entry";
     $attitude = $attitude_map[$f];
-    if (is_file($f) && $attitude === 'l') {
+    if (is_file($f) && $attitude !== 'd') {
         include 'entry.html';
     }
 }
